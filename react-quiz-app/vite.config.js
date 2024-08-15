eccc11b29604308b0f0317141e584c965b090e58
jimk-developer <jimk.developer@gmail.com>
2024-08-15 16:50:17 -0500

    deploy react to github

--- react-quiz-app/vite.config.js
+++ react-quiz-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-quiz-app/",
+  base: "/deploy-github/",
   plugins: [react()],
 })
